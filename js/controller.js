e99c3c4d9a5a0faa53d094d9aef77ddc3fe44381
LazyYang <teyzdota@gmail.com>
2019-05-03 00:31:10 +0800

    updated suggestion page code(navigator)

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,4 @@ angular.module('myApp')
 
 .controller('ManagerController', function($scope){
 
-    //Update Toolbar Title
-    $scope.pageTitle = "Add Transaction";
-    $scope.updateTitle = function(title){
-        $scope.pageTitle = title;
-    }
 });
